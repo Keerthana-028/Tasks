63c74043a32df2d60af957b3b1a354e9940db293
Keerthana-028 <keerthanar.bme2020@citchennai.net>
2024-08-21 17:24:43 +0530

    structure of the code is updated

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,13 @@
-def ispalindrome(a):
-    return a==a[::-1]
-a=input("enter the string:")
-output=ispalindrome(a)
-if output:
-    print('The given string is a palindrome')
-else:
-    print('It is not a palindrome')
 
+def is_palindrome(input):
+    return input == input[::-1]
+def main():
+    user_input = input("Enter the string:")
+    output = is_palindrome(input=user_input)
+    if output:
+        print('The given string is a palindrome.')
+    else:
+        print('It is not a palindrome.')
+
+if __name__ == "__main__":
+    main()
